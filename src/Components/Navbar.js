4df6b89868eae68f5abee4022a5d15d92f4075ec
shokes <oyatioshoke@yahoo.com>
2022-04-22 06:53:19 +0100

    addition of filter and toggle functionality for dark mode and light mode

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,44 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { MdOutlineWbSunny, MdModeNight } from 'react-icons/md';
 
 const Navbar = function () {
+  const [theme, setTheme] = useState('light-theme');
+
+  useEffect(() => {
+    document.documentElement.classList = theme;
+  }, [theme]);
+
+  const toggleHandler = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
   return (
     <section className='nav-background'>
       <div className='nav-bar container'>
-        <div>Country Info</div>
+        <Link to='/' className='home'>
+          Country Info
+        </Link>
         <div>
-          <Link to='/' className='home'>
-            Home
-          </Link>
           <Link to='/about' className='about'>
             About
           </Link>
+        </div>
+        <div onClick={toggleHandler}>
+          {theme === 'light-theme' ? (
+            <p className='theme-icon'>
+              <MdModeNight />
+              Dark Mode
+            </p>
+          ) : (
+            <p className='theme-icon'>
+              <MdOutlineWbSunny />
+              Light Mode
+            </p>
+          )}
         </div>
       </div>
     </section>
--- src/context.js
+++ src/context.js
@@ -2,7 +2,8 @@ import React, { useState, useContext, useEffect } from 'react';
 
 // creating global context
 
-const url = 'https://restcountries.com/v3.1/name/';
+const MainUrl = 'https://restcountries.com/v3.1/name/';
+const regionUrl = 'https://restcountries.com/v3.1/region/';
 
 const AppContext = React.createContext();
 
@@ -12,14 +13,21 @@ const AppProvider = function ({ children }) {
   const [loading, setLoading] = useState(true);
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('am');
+  const [region, setRegion] = useState('');
 
   const fetchCountries = async () => {
     setLoading(true);
+    let url;
+    if (region) {
+      url = `${regionUrl}${region}`;
+    } else {
+      url = `${MainUrl}${searchTerm}`;
+    }
     try {
-      const response = await fetch(`${url}${searchTerm}`);
+      const response = await fetch(`${url}`);
       const data = await response.json();
       const countriesData = data;
-      //console.log(countriesData);
+
       setLoading(false);
 
       if (countriesData) {
@@ -31,7 +39,7 @@ const AppProvider = function ({ children }) {
             region,
             flag: flags.png,
             name: name.common,
-            capital: capital[0],
+            capital: capital ? capital[0] : null,
           };
         });
 
@@ -40,18 +48,31 @@ const AppProvider = function ({ children }) {
         setCountries([]);
       }
     } catch (error) {
-      console.log(error);
       setLoading(false);
     }
   };
 
   useEffect(() => {
+    setRegion('');
     fetchCountries();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchTerm]);
+
+  useEffect(() => {
+    fetchCountries();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [region]);
 
   return (
     <AppContext.Provider
-      value={{ loading, setLoading, countries, setSearchTerm }}
+      value={{
+        loading,
+        setLoading,
+        region,
+        countries,
+        setRegion,
+        setSearchTerm,
+      }}
     >
       {children}
     </AppContext.Provider>
